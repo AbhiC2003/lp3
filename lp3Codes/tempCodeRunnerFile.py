f04d8558b8dd3c8c699ed7c3629756feebd9fadb
AbhiC2003 <125119674+AbhiC2003@users.noreply.github.com>
2024-11-06 10:26:28 +0530

    Add files via upload

---
+++ lp3Codes/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# Example usage:
+n = int(input("Enter the Number: "))  # Change this value for a different length of the sequence
+fib_sequence = fibonacci_Recursive(n)
+for num in fib_sequence:
+    print(num)
